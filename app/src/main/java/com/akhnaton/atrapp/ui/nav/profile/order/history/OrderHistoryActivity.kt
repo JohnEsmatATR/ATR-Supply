78b26862c7dae1f65141e89c6a6ef3539787ec20
Younan Adly <42420496+YounanAdly@users.noreply.github.com>
2024-03-24 12:58:50 +0200

    enhance

--- app/src/main/java/com/akhnaton/atrapp/ui/nav/profile/order/history/OrderHistoryActivity.kt
+++ app/src/main/java/com/akhnaton/atrapp/ui/nav/profile/order/history/OrderHistoryActivity.kt
@@ -1,19 +1,27 @@
 package com.akhnaton.atrapp.ui.nav.profile.order.history
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.akhnaton.atrapp.R
 import com.akhnaton.atrapp.data.model.orderHistory.OrderHistoryModel
 import com.akhnaton.atrapp.databinding.ActivityOrderHistoryBinding
 import com.akhnaton.atrapp.ui.nav.profile.order.details.OrderDetailsActivity
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
-class OrderHistoryActivity : AppCompatActivity(), OrderHistoryAdapter.OnProductClickListener {
+class OrderHistoryActivity : AppCompatActivity(), OrderHistoryAdapter.OnProductClickListener,
+    View.OnClickListener {
     private lateinit var binding: ActivityOrderHistoryBinding
     private var mAdapter = OrderHistoryAdapter()
     private var mList = mutableListOf<OrderHistoryModel>()
+    private val calendar = Calendar.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setupBinding()
@@ -29,24 +37,64 @@ class OrderHistoryActivity : AppCompatActivity(), OrderHistoryAdapter.OnProductC
         }
 
         binding.orderRecycler.adapter = mAdapter
+        binding.fromLayout.setOnClickListener(this)
+        binding.fromED.setOnClickListener(this)
+        binding.toLayout.setOnClickListener(this)
+        binding.toED.setOnClickListener(this)
     }
 
 
     private fun fillList() {
-        val l1 = OrderHistoryModel(1, 1000.0, "2", "1", "Giza, Egypt", "Pending",0)
-        val l2 = OrderHistoryModel(1, 1000.0, "2", "1", "Giza, Egypt", "Delivered",1)
-        val l3 = OrderHistoryModel(1, 1000.0, "2", "1", "Giza, Egypt", "Pending",0)
-        val l4 = OrderHistoryModel(1, 1000.0, "8", "6", "Giza, Egypt", "Delivered",1)
+        val l1 = OrderHistoryModel(1, 1000.0, "2", "1", "Giza, Egypt", "Pending", 0)
+        val l2 = OrderHistoryModel(1, 1000.0, "2", "1", "Giza, Egypt", "Delivered", 1)
+        val l3 = OrderHistoryModel(1, 1000.0, "2", "1", "Giza, Egypt", "Pending", 0)
+        val l4 = OrderHistoryModel(1, 1000.0, "8", "6", "Giza, Egypt", "Delivered", 1)
 
         mList.add(l1)
         mList.add(l2)
         mList.add(l3)
         mList.add(l4)
-        mAdapter.setData(mList,this)
+        mAdapter.setData(mList, this)
     }
 
     override fun onProductClick(data: OrderHistoryModel) {
         val intent = Intent(this, OrderDetailsActivity::class.java)
         startActivity(intent)
+    }
+
+    override fun onClick(v: View) {
+        showDatePicker(v.id)
+    }
+
+
+    private fun showDatePicker(id: Int) {
+        // Create a DatePickerDialog
+        val datePickerDialog = DatePickerDialog(
+            this,
+            R.style.CustomDatePickerDialog,
+            { _, year: Int, monthOfYear: Int, dayOfMonth: Int ->
+                // Create a new Calendar instance to hold the selected date
+                val selectedDate = Calendar.getInstance()
+                // Set the selected date using the values received from the DatePicker dialog
+                selectedDate.set(year, monthOfYear, dayOfMonth)
+                // Create a SimpleDateFormat to format the date as "dd/MM/yyyy"
+                val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+                // Format the selected date into a string
+                val formattedDate = dateFormat.format(selectedDate.time)
+                // Update the TextView to display the selected date with the "Selected Date: " prefix
+                if (binding.fromED.id == id) {
+                    binding.fromED.setText(formattedDate.toString())
+                } else {
+                    binding.toED.setText(formattedDate.toString())
+                }
+
+
+            },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+        // Show the DatePicker dialog
+        datePickerDialog.show()
     }
 }
